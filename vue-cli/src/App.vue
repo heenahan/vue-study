969a9149c92b5195df9fd47d7dfd426e9af28ac0
heenahan <mby1997s@gmail.com>
2024-05-07 22:12:54 +0900

    feat : vue 싱글 파일 컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,26 +1,41 @@
+<!-- 싱글 파일 컴포넌트 : 컴포넌트에 관련된 코드를 하나의 파일에서 관리 -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader v-bind:appTitle="header" v-on:change="changeMessage"/>
+  <hr>
+  <div>
+    {{ message }}
+  </div>
+  <!-- v-on:click 축약 @click-->
+  <button @click="showAlert">경고</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import 컴포넌트 이름 from './컴포넌트 파일 경로'
+  import AppHeader from './components/AppHeader.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      // '컴포넌트 이름' : 컴포넌트 내용
+      // 'AppHeader': AppHeader key와 value가 같으면 하나만 써도 됨
+      AppHeader
+    },
+    data() {
+      return {
+        message: 'Hello Vue!',
+        header: "App Header Component"
+      }
+    },
+    methods: {
+      showAlert() {
+        alert('Hello Vue!')
+      },
+      changeMessage() {
+        this.message = "Changed Message"
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
