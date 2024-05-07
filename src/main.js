088365d5f7228e04a2b3fbbc352c32043f30a83f
heenahan <mby1997s@gmail.com>
2024-05-07 20:33:08 +0900

    chore : vue-cli로 프로젝트 생성

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-// 싱글 파일 컴포넌트에서 최상위 컴포넌트 앱을 가져온다.
-import App from './App.vue'
-
-// 앱 마운트
-// 앱 인스턴스는 .mount() 메서드가 호출될 때까지 아무것도 렌더링하지 않는다.
-createApp(App).mount('#app') 
